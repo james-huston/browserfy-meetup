a97724c31c959ae453f791aacb2e1d02884e1fbb
James Huston <jhuston@redventures.com>
2015-06-29 20:35:26 -0400

    Added debug as a requirement. Put in code to add the local storage item needed for debugging. Rebuilt with changes.

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
 
+var debug = require('debug')('app');
+
 document.querySelector('#block-content').innerHTML = 'Hello All!';
+
+// enable debugging
+localStorage.setItem('debug', 'app*');
+
+// tell the world we are running!
+debug('App is running');
